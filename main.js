465d493d4ca3044eae21a5570903867306e7d0df
TUIRAN MENDOZA JOSE EDUARDO <jtuiran12@unisalle.edu.co>
2023-04-22 20:54:15 -0500

    Se añadio un menu para mobile

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const desktopMenu = document.querySelector(".desktop-menu")
 const navBarEmail = document.querySelector(".navbar-email")
 
+const mobileMenu = document.querySelector(".mobile-menu")
+const logoMenu = document.querySelector(".menu")
+
+
 navBarEmail.addEventListener("click", toggleDesktopMenu)
+logoMenu.addEventListener("click", toggleMobileMenu)
 
 function toggleDesktopMenu (){
     desktopMenu.classList.toggle("inactive")
+
+}
+function toggleMobileMenu (){ 
+    mobileMenu.classList.toggle("inactive")
+    console.log("Hola")
 
 }
